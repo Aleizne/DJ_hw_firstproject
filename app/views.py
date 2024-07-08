37f06265bb58e8e5a3783ea35b863a3e5b77b7b5
Aleizne <31164261+Aleizne@users.noreply.github.com>
2024-07-09 01:34:40 +0300

    Add files via upload

---
+++ app/views.py
@@ -0,0 +1,46 @@
+import os
+from datetime import datetime
+
+from django.http import HttpResponse
+from django.shortcuts import render, reverse
+
+
+def home_view(request):
+    template_name = 'app/home.html'
+    # впишите правильные адреса страниц, используя
+    # функцию `reverse`
+    pages = {
+        'Главная страница': reverse('home'),
+        'Показать текущее время': reverse('time'),
+        'Показать содержимое рабочей директории': reverse('workdir')
+    }
+    
+    # context и параметры render менять не нужно
+    # подбробнее о них мы поговорим на следующих лекциях
+    context = {
+        'pages': pages
+    }
+    return render(request, template_name, context)
+
+
+def time_view(request):
+    # обратите внимание – здесь HTML шаблона нет, 
+    # возвращается просто текст
+    current_time = datetime.now()
+    msg = f'Текущее время: {current_time}'
+    return HttpResponse(msg)
+
+
+def workdir_view(request):
+    # по аналогии с `time_view`, напишите код,
+    # который возвращает список файлов в рабочей 
+    # директории
+    res = sorted(os.listdir('.'))
+    msg = ("Рабочая папка содержит: <br/>" +
+           "<br/>".join(f'{number + 1}: {element}' for number, element in enumerate(res)))
+
+    # Упрощенная и адекватная альтернатива
+    # res = str(sorted(os.listdir()))
+    # msg = f'Рабочая папка содержит: {res}'
+    return HttpResponse(msg)
+    # raise NotImplemented
